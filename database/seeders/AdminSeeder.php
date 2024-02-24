bd8e238a7251c69e58da7acf29672d57f75d0703
Mark Benedict Abalos <benedictabalos30@gmail.com>
2024-02-24 13:07:07 +0800

    admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -12,6 +14,16 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'age' => 20,
+            'height' => '177.3',
+            'weight' => '69',
+            'gender' => 'male',
+            'password' => Hash::make('12345678'),
+            'role' => 'admin',
+            'status' => 'active',
+        ]);
     }
 }
