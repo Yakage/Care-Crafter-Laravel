26bbfcd28bfbbd14f5bbf429e5ba2356d74e910b
xChristian2 <aquino315532@gmail.com>
2024-02-29 05:08:27 +0800

    Added home scroll for history tracking front-end only

--- resources/views/user/user-ui/user.blade.php
+++ resources/views/user/user-ui/user.blade.php
@@ -1,12 +1,10 @@
-
-<!--Website: wwww.codingdung.com-->
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CodingDung | Profile Template</title>
+    <title>Edit Profile</title>
     <link rel="stylesheet" href="style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="css/account.css">
