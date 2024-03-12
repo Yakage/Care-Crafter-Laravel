5055435d63758b5d068c0da778a8afcd473a7035
Ivo Sorio <ivosorio77@gmail.com>
2024-03-12 22:52:58 +0800

    still fixing home- pls wag muna hawakan

--- resources/views/user/user-ui/user.blade.php
+++ resources/views/user/user-ui/user.blade.php
@@ -9,119 +9,47 @@
     <title>User Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        body {
-            min-height: 100vh;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background-color: #f8f9fa;
-            margin: 0;
-            background-image: url('/img/bg1.jpg');
-            background-attachment: fixed;
-            background-size: cover;
-        }
-
-        .container {
-            text-align: center;
-            background-color: #ffffff;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-            width: 100%;
-            max-width: 600px;
-        }
-
-        form {
-            width: 100%;
-        }
-
-        /* Custom style to position labels at the beginning */
-        .form-group label {
-            text-align: left;
-            display: block;
-            margin-bottom: 0.5rem;
-        }
-
-        /* Centering user information fields */
-        .user-info-fields {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        .form-group {
-            width: 100%;
-            max-width: 400px;
-            margin-bottom: 1rem;
-        }
-
-        /* Centering password fields */
-        .password-fields {
-            margin-left: auto;
-            margin-right: auto;
-            max-width: 400px;
-        }
-
-        /* Style for logout button */
-        .navbar-nav .nav-link {
-            color: black !important;
-            text-decoration: none !important;
-        }
-
-        .navbar-nav .nav-link:hover {
-            color: black !important;
-        }
-
-        .navbar-nav .btn-link {
-            color: black !important;
-            text-decoration: none !important;
-        }
-
-        .navbar-nav .btn-link:hover {
-            color: black !important;
-        }
-    </style>
+    <link rel="stylesheet" href="css/updateuser.css">
 </head>
 
 <body>
 <!--update users page-->
 <nav class="navbar bg-body-tertiary fixed-top">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src="{{ asset('img/CareCrafter-removebg-preview.png')}}" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
-                    CareCrafter
-                </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">CareCrafter Menu</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-            <div class="offcanvas-body">
-                <h4>Welcome, {{ $userData ['name'] }}!</h4>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('img/CareCrafter-removebg-preview.png')}}" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+                CareCrafter
+        </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">CareCrafter Menu</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+        <div class="offcanvas-body">
+            <h4>Welcome, {{ $userData ['name'] }}!</h4>
 
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('user.home')}}">Home</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Features</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Step Tracking</a></li>
-                            <li><a class="dropdown-item" href="#">Sleep Tracking</a></li>
-                            <li><a class="dropdown-item" href="#">Water Intake Reminder</a></li>
-                        </ul>
-                    </li>
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('user.home')}}">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Features</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Step Tracking</a></li>
+                        <li><a class="dropdown-item" href="#">Sleep Tracking</a></li>
+                        <li><a class="dropdown-item" href="#">Water Intake Reminder</a></li>
+                    </ul>
+                </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{route('user.leaderboards')}}">Leaderboards</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('user.feedback')}}">Feedback</a>
                     </li>
-                </ul>
+            </ul>
                 <div class="dropdown pb-4">
                     <a href="#" class="d-flex align-items-center text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                         <span class="d-none d-sm-inline mx-1 text-primary">{{ $userData ['name'] }}</span>
@@ -139,10 +67,12 @@
                     </ul>
                 </div>
             </div>
-            </div>
-            </div>
-        </nav>
-    <div>
+        </div>
+    </div>
+</nav>
+
+        
+    <div class="userdata">
     <h1>Welcome, {{ $userData['name'] }}!</h1>
     <p>Your email address is: {{ $userData['email'] }}</p>
 
@@ -177,7 +107,8 @@
                         <option value="female" @if ($userData['gender'] === 'female') selected @endif>Female</option>
                     </select>
                 </div>
-
+            </div>
+            <div class="changepass">
                 <h3>Change Password</h3>
                     <div class="form-group password-fields">
                         <label for="password">New Password</label>
@@ -189,7 +120,7 @@
                     </div>
 
                     <button type="submit" class="btn btn-primary" style="color: white; background-color: #56C2F2;">Update Profile</button>
-
+            </div>
             
         </form>
 
