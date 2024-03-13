b11533f9d2c379d1adc12c187f62fddab1cdb8e5
Mark Benedict Abalos <benedictabalos30@gmail.com>
2024-03-13 15:52:51 +0800

    Fix user feedback

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -8,21 +8,22 @@ use Illuminate\Support\Facades\Auth;
 
 class FeedbackController extends Controller{
     public function store(Request $request){
-        $user = Auth::user();
-        $request->validate([
-            'message' => 'required|min:5|max:700' // Validation rules
-        ], [
-            'message.max' => 'The message must not exceed 700 characters.'
-        ]);
-    
-        $feedback = new UserFeedback;
-        $feedback->name = auth()->name();
-        $feedback->message = $request->input('message');
-        $feedback->user_id = auth()->id(); // Assuming user authentication
-        $feedback->save();
+    $user = Auth::user();
+    $request->validate([
+        'message' => 'required|min:5|max:700' // Validation rules
+    ], [
+        'message.max' => 'The message must not exceed 700 characters.'
+    ]);
 
-        // Redirect to the 'user.feedback' view
-        return redirect()->route('user.feedback')->with('success', 'Feedback successfully submitted');
-    }
+    $feedback = new UserFeedback;
+    $feedback->name = $user->name; // Access the 'name' attribute directly
+    $feedback->message = $request->input('message');
+    $feedback->user_id = $user->id; // Assuming user authentication
+    $feedback->save();
+
+    // Redirect to the 'user.feedback' view
+    return redirect()->route('user.feedback')->with('success', 'Feedback successfully submitted');
+}
+
     
 }
