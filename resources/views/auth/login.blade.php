8c6d5247fbc795b22eed2d0a75e0804cf35a933a
Ivo Sorio <ivosorio77@gmail.com>
2024-03-15 22:48:47 +0800

    image in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,6 +19,7 @@
         @endif
     </div>
     <div class="container">
+        <img src= "{{ asset('img/CareCrafter-removebg-preview.png')}}" style="height: 25px; width: 25px;">
         <div class="title">Login to CareCrafter</div>
         <div class="content">
             <form id='loginForm' action="{{route('login')}}" method="POST">
